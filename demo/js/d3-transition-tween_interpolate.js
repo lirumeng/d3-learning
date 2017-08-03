a5dc73fdcd62226bf9c44f8397867d9e5d3edfd9
lirumeng <17317689023@163.com>
2017-08-03 09:32:02 +0800

    learning 3

--- demo/js/d3-transition-tween_interpolate.js
+++ demo/js/d3-transition-tween_interpolate.js
@@ -1,11 +1,17 @@
 var show = d3.select('#show');
+var cc = document.getElementById('color');
 
 show.transition()
-.duration(1500)
+.duration(2000)
 .tween('number', function(){
-  var i = d3.interpolateRound(0,100);
+  var i = d3.interpolateRound(0,1000);
+  var c = d3.interpolateRgb('#555','#fa0');
+
   return function(t){
     this.textContent = i(t);
+    d3.select(this).style('color',c(t));
+    console.log(cc.textContent);
+    cc.innerHTML = c(t);
   }
 });
 
@@ -21,11 +27,30 @@ var ci = d3.interpolateRgb('#f00', '#39e');
 var ai = d3.interpolateArray([0,1], [1,50,100]);
 var oi = d3.interpolateObject({x:0,y:1},{x:1,y:50,z:100});
 
+console.log('-----------------d3.interpolate-----------------');
 n.text(ni(0.3));
+console.log('-----------------d3.interpolate-----------------');
 s.text(si(0.3));
+console.log('-----------------d3.interpolateRgb-----------------');
 c.text(ci(0.6));
 c.style({
   'background':ci(0.6)
 });
+
+console.log('-----------------d3.interpolateArray-----------------');
 a.text(ai(0.5));
+
+console.log('-----------------d3.interpolateObject-----------------');
 console.log(oi(0.5));
+
+console.log('-----------------d3.interpolateTransform-----------------');
+var box = d3.select('#box');
+box.transition()
+.duration(2000).tween('move', function(){
+  var start = d3.transform('translate(10,10)');
+  var stop = d3.transform('translate(100,100)');
+  var interpolate = d3.interpolateTransform(start, stop);
+  return function(t){
+    d3.select(this).attr('transform',interpolate(t));
+  }
+})
